075b362433013dcae462af6c6bf9d65d0a44641a
devrafael-santos <raafael.cs@gmail.com>
2025-04-24 08:18:37 -0300

    first commit

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,6 +4,7 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
-    styleUrls: ['./header.component.css']
+    styleUrls: ['./header.component.css'],
+    standalone: false,
 })
 export class HeaderComponent {}
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,12 +1,10 @@
 import { Component, inject, Input } from '@angular/core';
-import { TaskComponent } from "./task/task.component";
-import { NewTaskComponent } from "./new-task/new-task.component";
 import { TasksService } from './tasks.service';
 @Component({
   selector: 'app-tasks',
-  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css'
+  styleUrl: './tasks.component.css',
+  standalone: false,
 })
 export class TasksComponent {
   @Input({ required: true }) userId!: string;
